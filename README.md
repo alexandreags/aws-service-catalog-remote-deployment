280a72dc8f051ba4a5a439e3793f9928ae0ac798
alexandre <alexandre.gomes@operacaomulticloud.com>
2023-06-02 15:14:09 -0300

    Fase1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aws-service-catalog-remote-deployment
 Solução para deployments Remotos em linked Accounts usando Service Catalog
+
+## Overview
+
+Apesar do ServiceCatalog suportar o deployment para contas linked em ambientes com AWS Organizations Habilitado, não é possivel utilitar paremetros customizados para cada deployment. 
+
+Essa solução usa um produto especifico dentro de um portfolio que usa Lambda e CF Custom resources para criar produtos provisionados nas contas das contas linkeds. 
+
+Suporta a remoção de produtos provisionados. 
+
+O deployment Centralizado concentra os produtos como versões do produto de deployment.
---
+++ src/setup-access-accounts-function/index_handler.py
@@ -0,0 +1,82 @@
+import boto3
+import os
+import cfnresponse
+import logging 
+import time
+from botocore.vendored import requests
+######
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+# Set Logger to stdout too
+logger.addHandler(logging.StreamHandler())
+
+####
+# Env Variables:
+
+DeployAccountId = os.eviron['DeployAccountId']
+
+AWS_REGION_NAME = 'us-east-1'
+
+org_client = boto3.client('organizations', region_name=AWS_REGION_NAME)
+
+def enable_service_access(Principal):
+    try:
+        logger.info(f'Habilitando Service Access {Principal}')
+        
+        service_access_response = org_client.enable_aws_service_access(
+            ServicePrincipal=Principal
+        )
+        
+        return service_access_response
+    
+    except Exception as e:
+        error_handler(e)
+
+def register_delegated_admin(AccountId,Service,event):
+    try:
+        if event['RequestType'] == 'Create':
+            logger.info(f'Registrando delegated Admin para conta {AccountId}')
+            delegated_admin_response = org_client.register_delegated_administrator(
+            AccountId=AccountId,
+            ServicePrincipal=Service
+            )
+            return delegated_admin_response
+        if event['RequestType'] == 'Delete':
+            logger.info(f'Removendo delegated Admin para conta {AccountId}')
+            delegated_admin_response = org_client.deregister_delegated_administrator(
+            AccountId=AccountId,
+            ServicePrincipal=Service
+            )
+            return delegated_admin_response
+    except Exception as e:
+        error_handler(e)
+
+def log(message, severity):
+    if severity == 'error':
+        logger.error(message)
+    else:
+        logger.info(message)
+        
+
+def error_handler(Ex,event,context):
+    responsedata = {}
+    responsedata['Erro'] = str(Ex)
+    log(f'### Ocorreu um erro durante o provisionamento: \n {responsedata}')
+    cfnresponse.send(event, context, cfnresponse.FAILED, responsedata) 
+
+
+
+def lambda_handler(event,context):
+    
+    if event['RequestType'] == 'Create':
+        
+        enable_service_access('servicecatalog.amazonaws.com')
+        enable_service_access('member.org.stacksets.cloudformation.amazonaws.com')
+        register_delegated_admin(DeployAccountId,'servicecatalog.amazonaws.com', event)
+        register_delegated_admin(DeployAccountId,'cloudformation.amazonaws.com', event)
+
+    if event['RequestType'] == 'Delete':
+
+        register_delegated_admin(DeployAccountId,'servicecatalog.amazonaws.com', event)
+        register_delegated_admin(DeployAccountId,'cloudformation.amazonaws.com', event)
